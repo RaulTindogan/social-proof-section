df60d74e9bf266ac3b186395b4fef21c643ccd79
RaulTindogan <tindoganraul@gmail.com>
2024-04-11 15:49:09 +0800

    update responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -2,44 +2,46 @@ import React from 'react'
 
 function App() {
   return (
-    <main className='py-10 px-5 text-Dark-Grayish-Magenta'>
-      <section className='text-center'>
-        <h1 className='text-4xl text-Very-Dark-Magenta font-[700] mb-5'>10,000+ of our users love our products.</h1>
-        <p className='font-[500]'> We only provide great products combined with excellent customer service. See what our satisfied customers are saying about our services.</p>
-      </section>
-      <section className='text-center py-10'>
-        <div className='p-3 bg-Light-Grayish-Magenta mb-5 rounded-lg'>
-          <div className='flex justify-center gap-2 mb-3'>
-            <img src="./src/assets/images/icon-star.svg" alt="" />
-            <img src="./src/assets/images/icon-star.svg" alt="" />
-            <img src="./src/assets/images/icon-star.svg" alt="" />
-            <img src="./src/assets/images/icon-star.svg" alt="" />
-            <img src="./src/assets/images/icon-star.svg" alt="" />
-          </div>
-          <p className='text-Very-Dark-Magenta font-[700]'>Rated 5 Stars in Reviews</p>
+    <main className='py-10 px-5 text-Dark-Grayish-Magenta sm:px-10 md:px-[4rem] sm:py-[4rem] max-w-[1440px]'>
+      <section className='lg:flex lg:justify-between'>
+        <div className='text-center sm:mb-5 lg:text-left lg:w-[50%] xl:pr-[12rem]'>
+          <h1 className='text-4xl text-Very-Dark-Magenta font-[700] mb-5 sm:text-5xl sm:mb-10 lg:mb-5'>10,000+ of our users love our products.</h1>
+          <p className='font-[500] sm:text-lg lg:text-[1rem]'> We only provide great products combined with excellent customer service. See what our satisfied customers are saying about our services.</p>
         </div>
-        <div className='p-3 bg-Light-Grayish-Magenta mb-5 rounded-lg'>
-          <div className='flex justify-center gap-2 mb-3'>
-            <img src="./src/assets/images/icon-star.svg" alt="" />
-            <img src="./src/assets/images/icon-star.svg" alt="" />
-            <img src="./src/assets/images/icon-star.svg" alt="" />
-            <img src="./src/assets/images/icon-star.svg" alt="" />
-            <img src="./src/assets/images/icon-star.svg" alt="" />
+        <div className='grid grid-cols-1 text-center py-10 sm:grid-cols-3 sm:gap-3 lg:w-1/2 lg:grid-cols-1'>
+          <div className='p-3 bg-Light-Grayish-Magenta mb-5 rounded-lg sm:px-3 sm:py-5 lg:flex lg:justify-center lg:mb-1 lg:items-center lg:py-2 lg:gap-10 lg:w-[400px]'>
+            <div className='flex justify-center gap-2 mb-3 lg:mb-0'>
+              <img src="./src/assets/images/icon-star.svg" alt="" />
+              <img src="./src/assets/images/icon-star.svg" alt="" />
+              <img src="./src/assets/images/icon-star.svg" alt="" />
+              <img src="./src/assets/images/icon-star.svg" alt="" />
+              <img src="./src/assets/images/icon-star.svg" alt="" />
+            </div>
+            <p className='text-Very-Dark-Magenta font-[700]'>Rated 5 Stars in Reviews</p>
           </div>
-          <p className='text-Very-Dark-Magenta font-[700]'>Rated 5 Stars in Report Guru</p>
-        </div>
-        <div className='p-3 bg-Light-Grayish-Magenta mb-5 rounded-lg'>
-          <div className='flex justify-center gap-2 mb-3'>
-            <img src="./src/assets/images/icon-star.svg" alt="" />
-            <img src="./src/assets/images/icon-star.svg" alt="" />
-            <img src="./src/assets/images/icon-star.svg" alt="" />
-            <img src="./src/assets/images/icon-star.svg" alt="" />
-            <img src="./src/assets/images/icon-star.svg" alt="" />
+          <div className='p-3 bg-Light-Grayish-Magenta mb-5 rounded-lg sm:px-3 sm:py-5 lg:flex lg:justify-center lg:mb-1 lg:items-center lg:py-2 lg:gap-10 px-10 lg:ml-[10%] lg:w-[400px]'>
+            <div className='flex justify-center gap-2 mb-3 lg:mb-0'>
+              <img src="./src/assets/images/icon-star.svg" alt="" />
+              <img src="./src/assets/images/icon-star.svg" alt="" />
+              <img src="./src/assets/images/icon-star.svg" alt="" />
+              <img src="./src/assets/images/icon-star.svg" alt="" />
+              <img src="./src/assets/images/icon-star.svg" alt="" />
+            </div>
+            <p className='text-Very-Dark-Magenta font-[700]'>Rated 5 Stars in Report Guru</p>
           </div>
-          <p className='text-Very-Dark-Magenta font-[700]'>Rated 5 Stars in BestTech</p>
-        </div>
-      </section>
-      <section className='text-White'>
+          <div className='p-3 bg-Light-Grayish-Magenta mb-5 rounded-lg sm:px-3 sm:py-5 lg:flex lg:justify-center lg:items-center lg:py-2 lg:gap-10 px-10 lg:ml-[20%] w-[400px]'>
+            <div className='flex justify-center gap-2 mb-3 lg:mb-0'>
+              <img src="./src/assets/images/icon-star.svg" alt="" />
+              <img src="./src/assets/images/icon-star.svg" alt="" />
+              <img src="./src/assets/images/icon-star.svg" alt="" />
+              <img src="./src/assets/images/icon-star.svg" alt="" />
+              <img src="./src/assets/images/icon-star.svg" alt="" />
+            </div>
+            <p className='text-Very-Dark-Magenta font-[700]'>Rated 5 Stars in BestTech</p>
+          </div>
+        </div>  
+      </section>     
+      <section className='text-White grid grid-cols-1 sm:grid-cols-2 gap-5 lg:grid-cols-3'>
         <div className='bg-Very-Dark-Magenta p-10 mb-5 rounded-lg'>
           <div className='flex gap-5 mb-5'>
             <img className='rounded-full w-[3rem]' src="./src/assets/images/image-colton.jpg" alt="Colton Smith" />
@@ -50,7 +52,7 @@ function App() {
           </div>
           <p>"We needed the same printed design as the one we had ordered a week prior. Not only did they find the original order, but we also received it in time. Excellent!"</p>
         </div>
-        <div className='bg-Very-Dark-Magenta p-10 mb-5 rounded-lg'>
+        <div className='bg-Very-Dark-Magenta p-10 mb-5 rounded-lg lg:mt-5'>
           <div className='flex gap-5 mb-5'>
             <img className='rounded-full w-[3rem]' src="./src/assets/images/image-irene.jpg" alt="Irene Roberts" />
             <div>
@@ -60,7 +62,7 @@ function App() {
           </div>
           <p>"Customer service is always excellent and very quick turn around. Completely delighted with the simplicity of the purchase and the speed of delivery."</p>
         </div>
-        <div className='bg-Very-Dark-Magenta p-10 mb-5 rounded-lg'>
+        <div className='bg-Very-Dark-Magenta p-10 mb-5 rounded-lg lg:mt-10'>
           <div className='flex gap-5 mb-5'>
             <img className='rounded-full w-[3rem]' src="./src/assets/images/image-anne.jpg" alt="Anne Wallace" />
             <div>
